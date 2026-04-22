0f1e43d95dc3d460e702c1d8a5572daefd523c34
codewithsathwik <sathvikms344@gmail.com>
2026-04-22 23:53:02 +0530

    added tuppel

---
+++ chapter6.py
@@ -0,0 +1,10 @@
+#tupels -> ()
+
+masala = ("cardamam", "cloves", "ciniamon", "Ginger")
+
+#unpack tuple
+(sp1, sp2, sp3, sp4) = masala
+print(f"Main spices : {sp1}, {sp2}, {sp3}")
+
+#membership operator
+print(f"IS ginger in masala? {"ginger" in masala}")  #case sensitive
